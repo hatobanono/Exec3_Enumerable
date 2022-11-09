6ccec40a0a54715022a6f6a6cac7718243b6c7f2
hatobanono <chris00iq@gmail.com>
2022-11-09 11:48:47 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			int[] items = Enumerable.Range(1, 10).ToArray();
+			foreach (var item in items)
+			{
+				Console.Write(item + " ");
+			}
+
+			Console.WriteLine();
+
+			int[] items2 = Enumerable.Range(10, 6).ToArray();
+			foreach (var item in items2)
+			{
+				Console.Write(item + " ");
+			}
+		}
+	}
+}
